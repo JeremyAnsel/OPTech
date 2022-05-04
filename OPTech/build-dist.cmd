a89eb0fe1f459b462c36790ca921afbc7a5352c0
Jérémy Ansel <JeremyAnsel@users.noreply.github.com>
2022-05-04 20:52:28 +0200

    Switch to net48

--- OPTech/build-dist.cmd
+++ OPTech/build-dist.cmd
@@ -4,12 +4,12 @@ setlocal
 cd "%~dp0"
 
 For %%a in (
-"OPTech\bin\Release\net45\*.dll"
-"OPTech\bin\Release\net45\*.exe"
-"OPTech\bin\Release\net45\*.exe.config"
-"OPTech\bin\Release\net45\*.bmp"
-"OPTech\bin\Release\net45\*.dat"
-"OPTech\bin\Release\net45\OPTech.pdb"
+"OPTech\bin\Release\net48\*.dll"
+"OPTech\bin\Release\net48\*.exe"
+"OPTech\bin\Release\net48\*.exe.config"
+"OPTech\bin\Release\net48\*.bmp"
+"OPTech\bin\Release\net48\*.dat"
+"OPTech\bin\Release\net48\OPTech.pdb"
 ) do (
 xcopy /s /d "%%~a" dist\
 )
--- OPTech/dist/_readme_.txt
+++ OPTech/dist/_readme_.txt
@@ -1,6 +1,6 @@
 
-OPTech runs on Windows 32/64 bits with the .NET framework 4.5.
-(Windows 7, Windows 8, Windows 10, or superior)
+OPTech runs on Windows 32/64 bits with the .NET framework 4.8.
+(Windows 7, Windows 8, Windows 10, Windows 11, or superior)
 
 
 OPTech commands:
